80537a033e01f471d36c0117ddd115592c62a6f6
okami-imako <barkan.makar@gmail.com>
2026-08-17 17:28:38 +0200

    ...

--- lua/custom/plugins/colorscheme.lua
+++ lua/custom/plugins/colorscheme.lua
@@ -24,14 +24,15 @@ return { -- You can easily change to a different colorscheme.
 				which_key = true,
 			},
 		})
-	end,
-	init = function()
-		-- Load the colorscheme here.
-		-- Like many other themes, this one has different styles, and you could load
-		-- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
 		vim.cmd.colorscheme("catppuccin")
-
-		-- You can configure highlights by doing something like:
-		vim.cmd.hi("Comment gui=none")
-	end,
+	end
+	-- init = function()
+	-- 	-- Load the colorscheme here.
+	-- 	-- Like many other themes, this one has different styles, and you could load
+	-- 	-- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
+	-- 	vim.cmd.colorscheme("catppuccin")
+	--
+	-- 	-- You can configure highlights by doing something like:
+	-- 	vim.cmd.hi("Comment gui=none")
+	-- end,
 }
--- lua/custom/plugins/lualine.lua
+++ lua/custom/plugins/lualine.lua
@@ -1,6 +1,7 @@
 return {
 	"nvim-lualine/lualine.nvim",
 	dependencies = { "nvim-tree/nvim-web-devicons" },
+	order = 1001,
 	config = function()
 		require("lualine").setup({
 			options = {
